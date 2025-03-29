93d58caf8675562c1a02671a8b76c4daadbbc2cc
OzanUlus <ilkerozanulus@gmail.com>
2025-03-29 18:10:06 +0300

    string methods

---
+++ Section5/string-methods.py
@@ -0,0 +1,29 @@
+msg = "Ozan Ulus, Course of Python"
+
+# result = msg.upper()
+# result = msg.lower()
+# result = msg.title()
+#result = msg.capatilize()
+
+# result = "abc".isupper()
+# result = "abc".islower()
+
+# result = msg.strip()
+# result = msg.split()
+# result = msg.split()[0]
+# result = msg.split(",")
+
+# result = msg.index("Ulus")
+# result = msg.startswith("O")
+# result = msg.startswith("n")
+result = msg.replace("Python", "C#")
+
+
+
+
+
+
+
+
+
+print(result)
