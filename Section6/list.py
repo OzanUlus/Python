689dce27521e01dadf9910391d7a4bb05639b96e
OzanUlus <ilkerozanulus@gmail.com>
2025-03-29 23:18:30 +0300

    list

---
+++ Section6/list.py
@@ -0,0 +1,30 @@
+
+organization = "Ozan Ulus".split()   #str to list
+print(type(organization))
+print(organization)
+print(organization[0])
+print(organization[1])
+
+
+numbers = [1,2,3,4,5]
+names = ["ali", "ahmet", "ayşe"]
+
+print(type(numbers))
+print(numbers[2])
+
+print(type(names))
+print(names[0])
+
+student = ["Ozan", "Ulus", 85,80,95]
+
+print(student[0] + " " + student[1])
+
+avg = (student[2] + student[3] + student[4]) / 3
+
+print(avg)
+
+students = [["Ozan", "Ulus", 85,80,95], ["Ahmet", "Taştekin", 80,80,90]]
+
+print(students[0][0])
+print(students[1][0])
+
